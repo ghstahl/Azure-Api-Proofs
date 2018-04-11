8af1df423b752eeb5c680eac39b07c6c469c2945
ghstahl@gmail.com <ghstahl@gmail.com>
2018-04-11 07:23:10 -0700

    reflecting claims from incoming token into our personal jwt

--- src/AuthHandler/GraphQL/TestAuthStore.cs
+++ src/AuthHandler/GraphQL/TestAuthStore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,20 +25,24 @@ namespace AuthHandler.GraphQL
         public async Task<BindResult> BindAsync(string type, string token)
         {
             ClaimsPrincipal cp;
+            Claim[] claims = null;
             switch (type)
             {
                 case "google-id_token":
                 case "norton-id_token":
                     // These will throw, but best to let it happen and catch all the way up top.
                     cp = await _jwtTokenValidation.ValidateToken(type,token);
+                    claims = cp.Claims.ToArray();
+                    break;
+                case "norton-seat_id":
+                case "norton-entitlement_id":
+                    claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, "bob")
+                    };
                     break;
             }
             
-
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, "bob")
-            };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var jwtToken = new JwtSecurityToken(
